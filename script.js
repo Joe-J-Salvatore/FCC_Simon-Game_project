ba9298495f2b6704a3140669b1ea8571b957f070
Joseph <14897294+Joe-J-Salvatore@users.noreply.github.com>
2018-01-05 22:51:59 -0800

    Added player ability, checks for winner and correct pattern clicks

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const simonSounds = ['http://freesound.org/data/previews/316/316901_5385832-lq.mp3', 'http://freesound.org/data/previews/316/316908_5385832-lq.mp3', 'http://freesound.org/data/previews/316/316906_5385832-lq.mp3', 'http://freesound.org/data/previews/316/316904_5385832-lq.mp3',
+	'http://freesound.org/data/previews/216/216090_3450800-lq.mp3'];
 const start = document.getElementById('start');
 const letters = document.querySelectorAll('span');
-console.log(letters);
-let simonSequence = [0, 1, 2];
+let simonSequence = [];
 let playerSequence = [];
+const numLevels = 3;
 let level = 0;
 
 start.addEventListener('click', function() {
@@ -12,26 +14,31 @@ start.addEventListener('click', function() {
 
 for (let j = 0; j < letters.length; j++) {
 	letters[j].addEventListener('click', function() {
-		let id = this.id;
+		let id = parseInt(this.id);
 		let color = this.classList.value;
 		playerSequence.push(id);
-		console.log(`User-clicked: ${id}, ${color}`);
+		console.log(`User-clicked: ${typeof id}, ${color}`);
 		addClassAndSound(id, color);
 		// verify user sequence matches Simon sequence
 		if (!checkPlayerSequence()) {
+			playSound(4);
 			displayError();
 			playerSequence = [];
 		}
-		if (playerSequence === simonSequence) {
+		if (playerSequence.length === simonSequence.length && playerSequence.length < numLevels) {
 			level++;
 			playerSequence = [];
 			startGameSequence();
+		}
+		if (playerSequence.length === numLevels) {
+			// display you win
 		}
 	});
 }
 
 function checkPlayerSequence() {
 	for (let m = 0; m < playerSequence.length; m++) {
+		console.log(`${simonSequence}, ${playerSequence}`);
 		if (playerSequence[m] !== simonSequence[m]) {
 			return false;
 		}
@@ -40,13 +47,24 @@ function checkPlayerSequence() {
 }
 
 function displayError() {
-	
+	console.log('error');
+	let counter = 0;
+	let errorInterval = setInterval(function() {
+		counter++;
+		if (counter === 3) {
+			// display error in HTML
+			// playSound(4);
+			clearInterval(errorInterval);
+			playerSequence = [];
+			counter = 0;
+		}
+	}, 500);
 }
 
 function startGameSequence() {
 	console.log(`Level: ${level}`);
 	// display level value in HTML
-	// generateRandomNum();
+	generateRandomNum();
 	let i = 0;
 	let gameInterval = setInterval(function() {
 		let id = simonSequence[i];
@@ -68,13 +86,14 @@ function generateRandomNum() {
 
 function addClassAndSound(id, color) {
 	document.getElementById(`${id}`).classList.add(`${color}-active`);
-	// playSound(id);
+	playSound(id);
 	setTimeout(function() {
 		document.getElementById(`${id}`).classList.remove(`${color}-active`);
 	}, 200);
 }
 
-// function playSound(id) {
-
-// }
+function playSound(id) {
+	let sound = new Audio(simonSounds[id]);
+	sound.play();
+}
 
